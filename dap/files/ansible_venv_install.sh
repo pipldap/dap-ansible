e8021fbad533960ef8b6e656fab60dab840490f0
dap_team <dap_team@pipl.com>
2018-03-13 07:28:57 -0500

    new file:   dap/files/airflow_agent_create.sh 	new file:   dap/files/ansible_venv_install.sh 	new file:   dap/files/etc/init.d/airflow_agent 	new file:   dap/files/opt/Spark/spark/conf/slaves 	new file:   dap/files/opt/Spark/spark/conf/spark-env.sh 	new file:   dap/tasks/create_airflow_agent.yml 	new file:   dap/tasks/create_dap_merge_venv.yml 	new file:   dap/tasks/install_spark.yml 	new file:   dap/tasks/main.yml 	new file:   dap/tasks/storage-box3_fstab.yml 	new file:   dap/templates/opt/Spark/spark/conf/spark-env.j2

---
+++ dap/files/ansible_venv_install.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+export http_proxy=http://proxy.pipl.pro:3128
+export https_proxy=$http_proxy
+export ftp_proxy=$http_proxy
+export dns_proxy=$http_proxy
+export rsync_proxy=$http_proxy
+
+cd /opt/airflow/airflow_bin/
+rm -rf venv_merge > /dev/null 2>&1
+
+STAT=
+virtualenv --no-site-packages -p /usr/bin/python venv_merge
+STAT=$STAT$?
+eval "$(ssh-agent -s)"
+STAT=$STAT$?
+ssh-add ~/.ssh/git_ssh
+STAT=$STAT$?
+
+#. venv_merge/bin/activate
+source venv_merge/bin/activate
+STAT=$STAT$?
+
+pip install wheel
+STAT=$STAT$?
+pip install git+ssh://git@github.com/piplcom/address_cleanup.git@development
+STAT=$STAT$?
+pip install git+ssh://git@github.com/piplcom/pypipl.git
+STAT=$STAT$?
+
+#pip install cassandra-driver==3.10 --install-option="--no-cython"
+pip install cassandra-driver==3.10
+STAT=$STAT$?
+
+cd venv_merge
+STAT=$STAT$?
+wget https://pypi.python.org/packages/source/d/distribute/distribute-0.6.14.tar.gz
+STAT=$STAT$?
+tar -xzf distribute-0.6.14.tar.gz
+STAT=$STAT$?
+cd distribute-0.6.14
+STAT=$STAT$?
+#python distribute-0.6.14/setup.py install
+python setup.py install
+STAT=$STAT$?
+
+git clone git@github.com:piplcom/data-acquisition.git
+STAT=$STAT$?
+
+export LIBRARY_PATH=/usr/local/lib/
+STAT=$STAT$?
+# pip install -r data-acquisition/requirements.txt
+
+REQ="
+MySQL-python==1.2.3
+SQLAlchemy==0.7.9
+anyjson==0.3.3
+argparse==1.2.1
+cassandra-driver==3.10
+celery==3.1.5
+celery-with-redis==3.0
+chardet
+elements
+lockfile==0.9.1
+lxml
+path.py==3.1
+phonenumbers==8.8.6
+pycassa==1.10.0
+pycrypto==2.4.1
+python-dateutil==1.5
+pytz==2013.8
+redis==2.7.6
+simplejson==3.2.0
+validate_email
+regex
+PyICU==1.8
+"
+
+
+for I in $REQ
+do
+    echo installing: $I
+    pip install $I
+    STAT=$STAT$?
+done
+
+
+echo Status $STAT
+exit $STAT
---
+++ dap/files/opt/Spark/spark/conf/spark-env.sh
@@ -0,0 +1,51 @@
+# - SPARK_PUBLIC_DNS, to set the public dns name of the driver program
+
+# Options read by executors and drivers running inside the cluster
+# - SPARK_LOCAL_IP, to set the IP address Spark binds to on this node
+# - SPARK_PUBLIC_DNS, to set the public DNS name of the driver program
+# - SPARK_LOCAL_DIRS, storage directories to use on this node for shuffle and RDD data
+# - MESOS_NATIVE_JAVA_LIBRARY, to point to your libmesos.so if you use Mesos
+
+# Options read in YARN client mode
+# - HADOOP_CONF_DIR, to point Spark towards Hadoop configuration files
+# - SPARK_EXECUTOR_CORES, Number of cores for the executors (Default: 1).
+# - SPARK_EXECUTOR_MEMORY, Memory per Executor (e.g. 1000M, 2G) (Default: 1G)
+# - SPARK_DRIVER_MEMORY, Memory for Driver (e.g. 1000M, 2G) (Default: 1G)
+
+# Options for the daemons used in the standalone deploy mode
+# - SPARK_MASTER_HOST, to bind the master to a different IP address or hostname
+# - SPARK_MASTER_PORT / SPARK_MASTER_WEBUI_PORT, to use non-default ports for the master
+# - SPARK_MASTER_OPTS, to set config properties only for the master (e.g. "-Dx=y")
+# - SPARK_WORKER_CORES, to set the number of cores to use on this machine
+# - SPARK_WORKER_MEMORY, to set how much total memory workers have to give executors (e.g. 1000m, 2g)
+# - SPARK_WORKER_PORT / SPARK_WORKER_WEBUI_PORT, to use non-default ports for the worker
+# - SPARK_WORKER_DIR, to set the working directory of worker processes
+# - SPARK_WORKER_OPTS, to set config properties only for the worker (e.g. "-Dx=y")
+# - SPARK_DAEMON_MEMORY, to allocate to the master, worker and history server themselves (default: 1g).
+# - SPARK_HISTORY_OPTS, to set config properties only for the history server (e.g. "-Dx=y")
+# - SPARK_SHUFFLE_OPTS, to set config properties only for the external shuffle service (e.g. "-Dx=y")
+# - SPARK_DAEMON_JAVA_OPTS, to set config properties for all daemons (e.g. "-Dx=y")
+# - SPARK_PUBLIC_DNS, to set the public dns name of the master or workers
+
+# Generic options for the daemons used in the standalone deploy mode
+# - SPARK_CONF_DIR      Alternate conf dir. (Default: ${SPARK_HOME}/conf)
+# - SPARK_LOG_DIR       Where log files are stored.  (Default: ${SPARK_HOME}/logs)
+# - SPARK_PID_DIR       Where the pid file is stored. (Default: /tmp)
+# - SPARK_IDENT_STRING  A string representing this instance of spark. (Default: $USER)
+# - SPARK_NICENESS      The scheduling priority for daemons. (Default: 0)
+# - SPARK_NO_DAEMONIZE  Run the proposed command in the foreground. It will not output a PID file.
+
+
+SPARK_MASTER_HOST=dap-mstream01.pipl.com
+SPARK_MASTER_PORT=7077
+SPARK_MASTER_WEBUI_PORT=8088
+SPARK_WORKER_CORES=7
+SPARK_WORKER_MEMORY=30G
+SPARK_WORKER_OPTS="-Dspark.worker.cleanup.enabled=true -Dspark.worker.cleanup.interval=86400"
+SPARK_HISTORY_OPTS="-Dspark.history.fs.logDirectory=/mnt/storage-box3/data/spark_data/spark-events/dap-mstream"
+
+SPARK_WORKER_DIR="/data1/spark_data/work"
+SPARK_LOCAL_DIRS="/data1/spark_data/tmp"
+SPARK_LOG_DIR="/data1/spark_data/logs"
+SPARK_PID_DIR="/data1/spark_data/tmp"
+
